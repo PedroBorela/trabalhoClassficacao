0174b7b7d93821cbb51feeadce03e416b92b6c7b
PedroBorela <142453565+PedroBorela@users.noreply.github.com>
2025-07-14 08:39:33 -0300

    app.py criado

--- app.py
+++ app.py
@@ -1,1 +1,22 @@
 import streamlit as st
+
+
+def main():
+    st.set_page_config(
+        page_title = "Classifica folhas de videiras!"
+    )
+    st.write("# Classifica folhas de videiras!")
+
+
+
+
+    #Carregar o modelo
+
+
+    #Carregar a imagem
+
+    #Classificar a imagem
+
+
+    if __name__ == "__main__":
+        main()
