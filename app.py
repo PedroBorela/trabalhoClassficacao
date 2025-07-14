f7ad7be54fc4c898f9ad9f1b46335010d9f25315
PedroBorela <142453565+PedroBorela@users.noreply.github.com>
2025-07-14 08:56:58 -0300

    carregar modelo

--- app.py
+++ app.py
@@ -1,5 +1,21 @@
 import streamlit as st
+import gdown
+import tensorflow as tf
 
+
+def carrega_modelo():
+    #https://drive.google.com/file/d/1GpWix8dp6FeFAs6g0etbnw_avu9Aflfp/view?usp=sharing
+    url = 'https://drive.google.com/uc?id=1GpWix8dp6FeFAs6g0etbnw_avu9Aflfp'
+
+    #baixa o arquivo (modelo)
+    gdown.down(url, 'modelo_quantizado16bits.tflite')
+
+    #carrega o modelo
+    interpreter = tf.lite.Interpreter(model_path = 'modelo_quantizado16bits.tflite' )
+
+    #disponibiliza para uso
+    interpreter.allocate_tensors()
+    return interpreter
 
 def main():
     st.set_page_config(
